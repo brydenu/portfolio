98ad48c08c3538928cca59007190a96ce109ec1a
brydenu <brydenu@gmail.com>
2023-12-19 09:29:39 -0800

    Added analytics to monitor traffic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Bryden Uyehara',
-  description: 'Portfolio site for Bryden Uyehara',
-  icons: {
-    icon: "/favicon.ico",
-    apple: '/apple-touch-icon.png',
-  }
-}
+    title: "Bryden Uyehara",
+    description: "Portfolio site for Bryden Uyehara",
+    icons: {
+        icon: "/favicon.ico",
+        apple: "/apple-touch-icon.png",
+    },
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    );
 }
